89ee9a7c6774c61e5dae50e89aa51f2b1f8e24eb
MagQ98 <maggieq.quinlan@gmail.com>
2022-02-10 17:23:10 +0000

    Added comment model to Admin section

--- sweettooth_app/admin.py
+++ sweettooth_app/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'status', 'author', 'created_on')
+    search_fields = ['title', 'content', 'ingredients']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content', 'ingredients', 'method')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'message', 'recipe', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'message')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
