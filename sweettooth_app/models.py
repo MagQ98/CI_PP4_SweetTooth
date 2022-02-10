d9094583519e726224e47d4587b3084e5c6bf105
MagQ98 <maggieq.quinlan@gmail.com>
2022-02-10 16:42:28 +0000

    Deployed and added Recipe and Comment models. Added admin panel

--- sweettooth_app/models.py
+++ sweettooth_app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipe"
+    )
+    updated_on = models.DateTimeField(auto_now=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    ingredients = models.TextField()
+    method = models.TextField()
+    prep_time = models.CharField(max_length=10,)
+    cook_time = models.CharField(max_length=10,)
+    servings = models.CharField(max_length=3,)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='recipe_likes', blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def _str_(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+class Comment(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    message = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.message} by {self.name}"
